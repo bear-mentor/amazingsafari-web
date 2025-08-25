5d88edb82b29a08d2c8b5e4c358a82df8b14a6ec
M Haidar Hanif <git@mhaidarhanif.com>
2025-08-25 21:18:32 +0700

    feat: routes register login dashboard

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,18 @@
-import { type RouteConfig, index, layout, route } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
   layout("./layouts/layout-main.tsx", [
     index("routes/home.tsx"),
-    route("products", "./routes/products.tsx"),
-    route("products/:slug", "./routes/products-slug.tsx"),
-    // ...
+    route("products", "routes/products.tsx"),
+    route("products/:slug", "routes/products-slug.tsx"),
+
+    route("register", "routes/register.tsx"), // POST /auth/register
+    route("login", "routes/login.tsx"), // POST /auth/login
+    route("dashboard", "routes/dashboard.tsx"), // GET /auth/me
   ]),
 ] satisfies RouteConfig;

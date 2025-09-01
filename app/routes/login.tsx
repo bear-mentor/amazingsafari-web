37d055b821ad33718ea4c3aba14d963521b0cbf6
M Haidar Hanif <git@mhaidarhanif.com>
2025-09-01 21:07:56 +0700

    feat: login client action

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,4 @@
-import { Form } from "react-router";
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/login";
 import { Label } from "~/components/ui/label";
 import { Input } from "~/components/ui/input";
@@ -6,6 +6,32 @@ import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Login" }];
+}
+
+export async function clientAction({ request }: Route.ClientActionArgs) {
+  const formData = await request.formData();
+
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const loginBody = {
+    email,
+    password,
+  };
+
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/auth/login`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(loginBody),
+    }
+  );
+  const result = await response.json();
+
+  console.log(result);
+
+  return redirect("/dashboard");
 }
 
 export default function LoginRoute({}: Route.ComponentProps) {
@@ -18,7 +44,7 @@ export default function LoginRoute({}: Route.ComponentProps) {
         </p>
       </header>
 
-      <Form method="post" className="space-y-4">
+      <Form method="POST" className="space-y-4">
         <div className="space-y-2">
           <Label htmlFor="email">Email</Label>
           <Input

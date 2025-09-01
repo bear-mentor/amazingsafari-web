9ace10b797a0b2fe4ee5292798d73ea6a98fa84b
M Haidar Hanif <git@mhaidarhanif.com>
2025-09-01 21:46:58 +0700

    feat: dashboard redirect to login if no token

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -2,6 +2,7 @@ import { getSession } from "~/sessions";
 import type { Route } from "./+types/dashboard";
 import type { UserAuthMe } from "~/modules/user/type";
 import { Card } from "~/components/ui/card";
+import { redirect } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "Dashboard" }];
@@ -9,7 +10,12 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({ request }: Route.ClientLoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
+  if (!session.has("token")) {
+    return redirect("/login");
+  }
+
   const token = session.get("token");
+
   const response = await fetch(
     `${import.meta.env.VITE_BACKEND_API_URL}/auth/me`,
     { headers: { Authorization: `Bearer ${token}` } }
